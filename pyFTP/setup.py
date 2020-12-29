f7765efd834072e27a94587ca55651a59b53c64c
AsajuHuishi <AsajuHuishi@github.com>
2020-12-30 00:36:48 +0800

    0add all

---
+++ pyFTP/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+setuptools.setup(name='pyftp',
+      version='0.0.1',
+      description='FTP file transfer and management by Python',
+      url='github项目地址 https://github.com/AsajuHuishi/FTP_file_transfer_management',
+      author='AsajuHuishi',
+      author_email='zengxinyang@tju.edu.cn',
+      packages=setuptools.find_packages(),
+      zip_safe=False)
