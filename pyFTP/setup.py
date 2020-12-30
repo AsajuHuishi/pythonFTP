f98bff56b059648f6e4cf2d0df3d22559071da98
AsajuHuishi <AsajuHuishi@github.com>
2020-12-30 16:06:31 +0800

    add

--- pyFTP/setup.py
+++ pyFTP/setup.py
@@ -1,9 +1,45 @@
 import setuptools
-setuptools.setup(name='pyFTP',
-      version='0.0.1',
-      description='FTP file transfer and management by Python',
-      url='github项目地址 https://github.com/AsajuHuishi/FTP_file_transfer_management',
-      author='AsajuHuishi',
-      author_email='zengxinyang@tju.edu.cn',
-      packages=setuptools.find_packages(),
-      zip_safe=False)
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
+    
+    
+setuptools.setup(name='py-FTP',
+    version='0.0.1',
+    description='FTP file transfer and management by Python',
+    url='https://github.com/AsajuHuishi/FTP_file_transfer_management',
+    author='AsajuHuishi',
+    author_email='zengxinyang@tju.edu.cn',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    zip_safe=False)
+      
+# import setuptools
+
+# with open("README.md", "r", encoding="utf-8") as fh:
+    # long_description = fh.read()
+
+# setuptools.setup(
+    # name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    # version="0.0.1",
+    # author="Example Author",
+    # author_email="author@example.com",
+    # description="A small example package",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    # url="https://github.com/pypa/sampleproject",
+    # packages=setuptools.find_packages(),
+    # classifiers=[
+        # "Programming Language :: Python :: 3",
+        # "License :: OSI Approved :: MIT License",
+        # "Operating System :: OS Independent",
+    # ],
+    # python_requires='>=3.6',
+# )
